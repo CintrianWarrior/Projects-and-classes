937354fec98ad47745d68b173aaf24ce1cfaf1ca
CintrianWarrior <velegoj2@yandex.ru>
2022-11-01 17:10:23 +0300

    Homework2

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author authorName;
     private int publicationYear;
 
-    public Book (String bookName, Author authorName, int publicationYear) {
+    public Book(String bookName, Author authorName, int publicationYear) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.publicationYear = publicationYear;
@@ -12,15 +14,33 @@ public class Book {
     public String getBookName() {
         return this.bookName;
     }
+
     public Author getAuthorName() {
         return this.authorName;
     }
 
     public int getPublicationYear() {
-        return  this.publicationYear;
+        return this.publicationYear;
     }
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + bookName + "." + " Имя автора - " + authorName + "." + " Года публикации - " + publicationYear + ".";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && bookName.equals(book.bookName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, publicationYear);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,19 @@ public class Main {
         String separator = ".................................";
         System.out.println(separator);
     }
+
     public static void main(String[] args) {
         Author firstAuthor = new Author("Джек", "Лондон");
         Book firstBook = new Book("Мартин Иден", firstAuthor, 1909);
-        System.out.println("Название книги - " + firstBook.getBookName());
-        System.out.println("Имя автора - " + firstBook.getAuthorName().getFirstName() + " " + firstBook.getAuthorName().getSecondName());
-        System.out.println("Год публикации - " + firstBook.getPublicationYear());
+        System.out.println(firstBook);
         printSeparator();
         Author secondAuthor = new Author("Джордж", "Оруэлл");
         Book secondBook = new Book("Дни в Бирме", secondAuthor, 1934);
-        System.out.println("Название книги - " + secondBook.getBookName());
-        System.out.println("Имя автора - " + secondBook.getAuthorName().getFirstName() + " " + secondBook.getAuthorName().getSecondName());
-        System.out.println("Год публикации - " + secondBook.getPublicationYear());
+        System.out.println(secondBook);
         printSeparator();
         Author thirdAuthor = new Author("Эрнест", "Хемингуэй");
         Book thirdBook = new Book("Острова в океане", secondAuthor, 2022);
-        System.out.println("Название книги - " + thirdBook.getBookName());
-        System.out.println("Имя автора - " + thirdBook.getAuthorName().getFirstName() + " " + thirdBook.getAuthorName().getSecondName());
-        System.out.println("Год публикации - " + thirdBook.getPublicationYear());
+        System.out.println(thirdBook);
         thirdBook.setPublicationYear(1970);
         System.out.println("Настоящий год публикации - " + thirdBook.getPublicationYear());
     }
